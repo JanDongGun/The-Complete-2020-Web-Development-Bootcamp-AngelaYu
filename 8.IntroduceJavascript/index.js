942710318e0f25eecb6e6c6b7f2e4b7937ad2fce
jandonggun <donggontrannguyen@gmail.com>
2020-12-02 10:12:38 +0700

    finish introduce js

---
+++ 8.IntroduceJavascript/index.js
@@ -0,0 +1,26 @@
+// var yourName = prompt("What is your name");
+// var firstCharOfName = yourName.slice(0, 1);
+// var remainOfName = yourName.slice(1, yourName.length);
+// var upperFirstChar = firstCharOfName.toUpperCase();
+// alert("hello " + upperFirstChar + remainOfName);
+
+// var a = 5 % 8;
+// console.log(a);
+
+// function print(){
+//     console.log("donggon");
+// }
+
+// function cal(a, b){
+//     return a + b;
+// }
+
+// console.log(cal(5, 6)); 
+
+// function bmiCalculator(weight, height){
+//     return Math.floor(weight / Math.pow(height, 2));
+// }
+
+// console.log(bmiCalculator(65, 1.8));
+
+console.log(Math.floor(2.7));
