0c6073758d34ad9c12f60026030b546d55823a5a
jandonggun <donggontrannguyen@gmail.com>
2020-12-04 09:32:49 +0700

    finish nodeJs

---
+++ 17.NodeJS/index.js
@@ -0,0 +1,9 @@
+// const fs = require("fs");
+
+// fs.copyFileSync("text1.txt", "text2.txt");
+
+const suppervillains = require("supervillains");
+
+let mySupperVillains = suppervillains.random();
+
+console.log(mySupperVillains);
